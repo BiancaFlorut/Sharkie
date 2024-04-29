1eecf505b2bd05c8ece139dc43dbc2309d14f16c
Bianca Floruț <150796482+BiancaFlorut@users.noreply.github.com>
2024-04-29 14:27:41 +0200

    backgrounds

--- js/models/world.class.js
+++ js/models/world.class.js
@@ -1,15 +1,10 @@
 class World {
     ctx;
     sharkie = new Character();
-    enemies = [new PufferFish(), new PufferFish(), new PufferFish()];
-    backgrounds = [
-        new Background("../img/3. Background/Layers/5. Water/L1.png", 0, 0 , 720, 480), 
-        new Background("img/3. Background/Layers/4.Fondo 2/L1.png", 0, 0, 720, 480), 
-        new Background("img/3. Background/Layers/3.Fondo 1/L1.png", 0, 0, 720, 480) ,
-        new Background("img/3. Background/Layers/2. Floor/L1.png    ", 0, 0, 720, 480) ,
-        new Light()
-    ];
+    enemies = LEVEL1.enemies;
+    backgrounds = LEVEL1.backgrounds;
     keyboard;
+    camera_x = 0;
 
     constructor(canvas, keyboard) {
         this.keyboard = keyboard;
@@ -24,9 +19,11 @@ class World {
 
     draw() {
         this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+        this.ctx.translate(this.camera_x, 0);
         this.drawObjects(this.backgrounds);
         this.addToWorld(this.sharkie);
         this.drawObjects(this.enemies);
+        this.ctx.translate(-this.camera_x, 0);
 
         let self = this;
         requestAnimationFrame(() => self.draw());
@@ -40,16 +37,22 @@ class World {
 
     addToWorld(movableObject) {
         if (movableObject.otherDirection) {
-            this.ctx.save();
+            this.flipObject(movableObject);
+        }
+        this.ctx.drawImage(movableObject.img, movableObject.x, movableObject.y, movableObject.width, movableObject.height);
+        if (movableObject.otherDirection) {
+            this.flipObjectBack(movableObject);
+        }
+    }
+
+    flipObject(movableObject){
+        this.ctx.save();
             this.ctx.translate(movableObject.width, 0);
             this.ctx.scale(-1, 1); 
             movableObject.x = movableObject.x * -1;
-        }
-        
-        this.ctx.drawImage(movableObject.img, movableObject.x, movableObject.y, movableObject.width, movableObject.height);
-        if (movableObject.otherDirection) {
-            this.ctx.restore();
-            movableObject.x = movableObject.x * -1;
-        }
+    }
+    flipObjectBack(movableObject){
+        this.ctx.restore();
+        movableObject.x = movableObject.x * -1;
     }
 }
